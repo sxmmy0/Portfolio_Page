0d875d8904421e4a4af7d8ce6d526c4daac9dd79
sxmmy0 <samobembe97@gmail.com>
2025-01-08 17:09:20 +0000

    Updated to add tape and about to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import { Header } from "@/sections/Header";
 import { HeroSection  } from "@/sections/Hero";
 import { ProjectsSection } from "@/sections/Projects";
+import { TapeSection } from "@/sections/Tape";
+import { AboutSection } from "@/sections/About";
 
 export default function Home() {
   return (
     <div>
       <Header />
       <HeroSection />
+      <AboutSection />
+      <TapeSection />
       <ProjectsSection />
     </div>
   );
